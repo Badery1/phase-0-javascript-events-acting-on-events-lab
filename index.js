8eb1ba325fd3591dd4bd41abab4798555d20571a
barit <mmyers.com@Gmail.com>
2023-08-12 01:58:56 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+const containerWidth = 400
+const dodgerWidth = 40
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+
+    const maxLeft = containerWidth - dodgerWidth;
+    if (left < maxLeft) {
+        dodger.style.left = `${left + 1}px`;
+  }
+}
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
